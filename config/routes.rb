60040ff1ea76169621dd9956ab24447c4492fda0
shioka.yahagi <0829shiok@gmail.com>
2019-10-12 18:48:54 +0000

    ログイン機能を作りました

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,20 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
   get 'users/index'
   get 'users/show'
   get 'users/new'
   get 'users/create'
   root to: 'tasks#index'
   
+  #ログイン機能関連のルーティング
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
   #user関連のルーティング
   get 'signup', to: 'users#new'
-  resources :users, only: [:index, :show, :create]
+  resources :users, only: [:index, :show, :new, :create]
 
 end
