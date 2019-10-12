c32f851af364c4af6c1e364111b00c0918831288
shioka.yahagi <0829shiok@gmail.com>
2019-10-12 18:07:49 +0000

    createを作った

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,12 +5,28 @@ class UsersController < ApplicationController
   def show
   end
 
-  def new
-  @user = User.new
   #新規ユーザ登録ボタンを押したら、このNew
   #メソッドに来て、new.html.erb(ユーザ登録画面)に遷移していく。
+  def new
+  @user = User.new
   end
 
+#ユーザー登録画面からPOSTされた情報を受け取るメソッド
   def create
+    @user = User.new(user_params) #user_paramsはデータをチェックするメソッド
+    
+    if @user.save
+      flash[:success] = '新しいユーザーを登録しました'
+      redirect_to @user
+    else
+      flash[:danger] = 'ユーザーの登録に失敗しました'
+      render :new
+    end
   end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end 
 end
