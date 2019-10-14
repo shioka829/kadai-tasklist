5fce4f1c6c2ff9bd0b3dac030b44268191051a9f
shioka.yahagi <0829shiok@gmail.com>
2019-10-14 08:25:54 +0000

    ルーティングの修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,6 @@ Rails.application.routes.draw do
   get 'sessions/new'
   get 'sessions/create'
   get 'sessions/destroy'
-  get 'users/index'
-  get 'users/show'
-  get 'users/new'
-  get 'users/create'
-  post 'tasks/create'
   root to: 'tasks#index'
   
   #ログイン機能関連のルーティング
