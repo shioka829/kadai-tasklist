1d28adbb8a6e77c5970b036b2fb9c156bc6d3708
shioka.yahagi <0829shiok@gmail.com>
2019-10-15 06:49:46 +0000

    ルーティングの修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,5 @@
 class UsersController < ApplicationController
   before_action :require_user_logged_in, only: [:index, :show]
-  
-  def index
-  end
-
-  #ユーザ情報の表示
-  def show
-    @user = User.find(params[:id])
-    @tasks = @user.tasks.order(id: :desc).page(params[:page])
-    counts(@user)
-  end
 
   #新規ユーザ登録ボタンを押したら、このNew
   #メソッドに来て、new.html.erb(ユーザ登録画面)に遷移していく。
